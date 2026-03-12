8363638a44e75c6076daeb4eff7f0df0dfa9e11d
Dan McNerthney <dan@softllc.com>
2026-03-12 08:02:31 -0700

    fix local websocket

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,21 @@ export default defineConfig({
         target: 'http://localhost:4000',
         changeOrigin: true,
       },
+      // Proxy /socket.io to backend
+      '/socket.io': {
+        target: 'ws://localhost:4000', // Your backend WS server
+        ws: true, // Enable WebSocket proxying
+        changeOrigin: true
+      }//,
+      // Example: Proxy another websocket endpoint
+      //'/ws': {
+      //  target: 'ws://localhost:8078',
+      //  ws: true
+      // }
     },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
+    preview: {
+      host: '0.0.0.0',
+      port: 3000,
+    },
+  }
 })
